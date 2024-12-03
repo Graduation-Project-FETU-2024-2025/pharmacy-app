196d9262f1a97b4613c4037273d7452a4fb8c605
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-12-03 07:41:23 +0200

    edit route

--- lib/core/routers/routing.dart
+++ lib/core/routers/routing.dart
@@ -1,13 +1,4 @@
 abstract class Routing {
   
   static const String onboarding = '/onboarding';
-  static const String signUp = '/signUp';
-  static const String login = '/login';
-  static const String home = '/home';
-  static const String profile = '/profile';
-  static const String forgetPassword = '/forgetPassword';
-  static const String enterOTP = '/enterOTP';
-  static const String restPassword = '/restPassword';
-  static const String savedDonations = '/savedDonations';
-  static const String donationDetails = '/donationDetails';
 }
