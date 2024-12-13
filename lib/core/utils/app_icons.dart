1662864540ac972bfa553b0ecb41591be20073b8
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-12-13 22:46:17 +0200

    add app icons

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_single_quotes
+class AppIcons {
+  AppIcons._();
+  
+  /// Assets for iconsAlarm
+  /// assets/icons/alarm.svg
+  static const String iconsAlarm = "assets/icons/alarm.svg";
+
+  /// Assets for iconsAuthHeader
+  /// assets/icons/auth_header.svg
+  static const String iconsAuthHeader = "assets/icons/auth_header.svg";
+
+  /// Assets for iconsBack
+  /// assets/icons/back.svg
+  static const String iconsBack = "assets/icons/back.svg";
+
+  /// Assets for iconsBranches
+  /// assets/icons/branches.svg
+  static const String iconsBranches = "assets/icons/branches.svg";
+
+  /// Assets for iconsCamera
+  /// assets/icons/camera.svg
+  static const String iconsCamera = "assets/icons/camera.svg";
+
+  /// Assets for iconsClock
+  /// assets/icons/clock.svg
+  static const String iconsClock = "assets/icons/clock.svg";
+
+  /// Assets for iconsDrugs
+  /// assets/icons/drugs.svg
+  static const String iconsDrugs = "assets/icons/drugs.svg";
+
+  /// Assets for iconsEdit
+  /// assets/icons/edit.svg
+  static const String iconsEdit = "assets/icons/edit.svg";
+
+  /// Assets for iconsEmail
+  /// assets/icons/email.svg
+  static const String iconsEmail = "assets/icons/email.svg";
+
+  /// Assets for iconsError
+  /// assets/icons/error.svg
+  static const String iconsError = "assets/icons/error.svg";
+
+  /// Assets for iconsExit
+  /// assets/icons/exit.svg
+  static const String iconsExit = "assets/icons/exit.svg";
+
+  /// Assets for iconsHome
+  /// assets/icons/home.svg
+  static const String iconsHome = "assets/icons/home.svg";
+
+  /// Assets for iconsLocation
+  /// assets/icons/location.svg
+  static const String iconsLocation = "assets/icons/location.svg";
+
+  /// Assets for iconsMoney
+  /// assets/icons/money.svg
+  static const String iconsMoney = "assets/icons/money.svg";
+
+  /// Assets for iconsNoResult
+  /// assets/icons/no_result.svg
+  static const String iconsNoResult = "assets/icons/no_result.svg";
+
+  /// Assets for iconsNotification
+  /// assets/icons/notification.svg
+  static const String iconsNotification = "assets/icons/notification.svg";
+
+  /// Assets for iconsOnboarding
+  /// assets/icons/onboarding.svg
+  static const String iconsOnboarding = "assets/icons/onboarding.svg";
+
+  /// Assets for iconsProfile
+  /// assets/icons/profile.svg
+  static const String iconsProfile = "assets/icons/profile.svg";
+
+  /// Assets for iconsSearch
+  /// assets/icons/search.svg
+  static const String iconsSearch = "assets/icons/search.svg";
+
+  /// Assets for iconsVerified
+  /// assets/icons/verified.svg
+  static const String iconsVerified = "assets/icons/verified.svg";
+}
+
