f9ff5a45a3f365e6904c85d5ceef3bdddecd2e1d
khaledgamal22 <kghaly56@gmail.com>
2024-12-16 18:08:49 +0200

    add translation

--- lib/features/all_medicine/presentation/views/widgets/search_text_field.dart
+++ lib/features/all_medicine/presentation/views/widgets/search_text_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_icons.dart';
 
