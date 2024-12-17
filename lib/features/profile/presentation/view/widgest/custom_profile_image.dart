32c83763b334994e14ceb0feee369ec94c12a1df
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-12-17 11:44:09 +0200

    remove unused import

--- lib/features/profile/presentation/view/widgest/custom_profile_image.dart
+++ lib/features/profile/presentation/view/widgest/custom_profile_image.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pharmacy_app/core/utils/app_colors.dart';
 import 'package:pharmacy_app/core/utils/app_icons.dart';
-import 'package:pharmacy_app/core/utils/app_images.dart';
 
 class CustomProfileImage extends StatelessWidget {
   const CustomProfileImage({
