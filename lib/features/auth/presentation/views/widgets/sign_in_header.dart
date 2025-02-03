29ae926586ebf4afac621033b240b19c670ac602
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-02-03 08:25:13 +0200

    refactor sign in view by use custom scroll view and make fancy scrolling

--- lib/features/auth/presentation/views/widgets/sign_in_header.dart
+++ lib/features/auth/presentation/views/widgets/sign_in_header.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pharmacy_app/core/utils/app_icons.dart';
 import 'package:pharmacy_app/core/utils/app_images.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignInHeader extends StatelessWidget {
   const SignInHeader({
@@ -12,24 +13,24 @@ class SignInHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         canBack
             ? IconButton(
                 icon: SvgPicture.asset(
                   AppIcons.iconsBack,
-                  height: 50,
                 ),
                 onPressed: () {
                   Navigator.pop(context);
                 },
               )
-            : SizedBox(),
-        Spacer(),
+            : SizedBox.shrink(),
+
         Image.asset(
           AppImages.imgAuthHeader,
-          height: 150,
+          height: 100.h,
         ),
-        Spacer(),
+        // Spacer(),
       ],
     );
   }
