6a7b3b7e54dfbfb57600640489204d40d6c318c5
Dina Ahmed <denaahmed340@gmail.com>
2025-03-05 07:20:35 +0200

    delete logic

--- lib/features/pharmacy_details/presentation/view_model/cubit/get_branches/get_branches_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'get_branches_state.dart';
-
-class GetBranchesCubit extends Cubit<GetBranchesState> {
-  GetBranchesCubit() : super(GetBranchesInitial());
-}
