b1012bdacabefb443ef297f5d2ccf16851cf748f
khaledgamal22 <kghaly56@gmail.com>
2025-03-22 16:53:05 +0200

    fix errors

--- lib/features/all_medicines/data/repos/get_branch_products_repo_impl.dart
+++ lib/features/all_medicines/data/repos/get_branch_products_repo_impl.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:pharmacy_app/core/database/api/api_consumer.dart';
 import 'package:pharmacy_app/core/database/api/end_points.dart';
 import 'package:pharmacy_app/core/database/cache/cashe_helper.dart';
-import 'package:pharmacy_app/core/database/cache/secure_storage.dart';
 import 'package:pharmacy_app/core/services/get_it.dart';
 import 'package:pharmacy_app/features/all_medicines/data/models/get_branch_products_model.dart';
 
@@ -17,20 +16,18 @@ class GetBranchProductsRepoImpl implements GetBranchProductsRepo {
     required String branchId,
   }) async {
     try {
-      String? token = await SecureStorage.instance.getData(key: ApiKeys.token);
-      if (token == null) throw Exception("Authentication token not found");
-      String lang = getIt.get<CacheHelper>().getCurrentLanguage();
       final response = await apiConsumer.get(
         "${EndPoints.getBranchProducts}/$branchId",
-        headers: {
-          ApiKeys.token: token,
-          ApiKeys.lang: lang,
-        },
         queryParameter: {
           "page": 1,
           "pageSize": 10,
         },
       );
+      if (response.statusCode == 204) {
+        return left(getIt<CacheHelper>().getCurrentLanguage() == 'ar'
+            ? 'لا يوجد أدوية في الفرع'
+            : 'No Medicines in Branch');
+      }
       final data = GetBranchProductsModel.fromJson(response.data);
       return Right(data);
     } catch (e) {
